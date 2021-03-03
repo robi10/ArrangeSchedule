afbbc3527617059bb59b06d32b61c8c02f1991a7
robi10 <robi10@kg8.so-net.ne.jp>
2021-03-03 09:13:20 +0900

    Add files via upload

---
+++ src/main/java/com/tuyano/springboot/ArrangeScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.tuyano.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrangeScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrangeScheduleApplication.class, args);
+	}
+
+}
